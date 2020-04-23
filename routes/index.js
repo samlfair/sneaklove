0ae4dbbcd00f824dfb350e703846dc0984d4d6dc
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-04-23 16:15:08 +0200

    Set product and auth view routes

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const Sneaker = require("../models/Sneaker");
+const Tag = require("../models/Tag");
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
 router.get("/sneakers/:cat", (req, res) => {
-  res.send("bar");
+  renderCategoryPage(req, res);
 });
+
+async function renderCategoryPage(req, res) {
+  categoryId = req.params.cat;
+  try {
+    const dbResSneaker = await Sneaker.find({ tag_id: categoryId }); // not sure if this is working
+    const dbResTag = await Tag.find({});
+    const dbResCat = await Tag.findById(categoryId);
+    res.render("products", {
+      sneakers: dbResSneaker,
+      category: dbResCat.label,
+      tags: dbResTag,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 router.get("/one-product/:id", (req, res) => {
   res.send("baz");
 });
 
 router.get("/signup", (req, res) => {
-  res.send("sneak");
+  res.render("signin");
 });
 
 router.get("/signin", (req, res) => {
-  res.send("love");
+  res.render("signup");
 });
 
 module.exports = router;
